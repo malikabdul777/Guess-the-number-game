3ff819b3adf2c0ed0c9520b302fe97c6f7e8bbdf
malikabdul777 <malik7771@yahoo.com>
2021-09-18 20:23:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const displayMessage = function (message) {
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.guess').value = '';
-  document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('.number').textContent = '?';
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
